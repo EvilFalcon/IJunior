ce013a7aaf205b52b976d1179efe4250436395f0
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2024-01-31 00:09:46 +0300

    refactor

--- Assets/New Folder/Sorses/Cube.cs
+++ Assets/New Folder/Sorses/Cube.cs
@@ -3,16 +3,10 @@ using UnityEngine;
 
 public class Cube : MovementComponentBase
 {
-    private Vector3 _vector3;
     [SerializeField, Range(0, 30)] private float _speed;
-
-    private void Awake()
-    {
-        _vector3 = Vector3.up;
-    }
 
     protected override void Move()
     {
-        transform.Rotate(_vector3 * (Time.deltaTime * _speed));
+        transform.Rotate(Vector3.up * (Time.deltaTime * _speed));
     }
 }
