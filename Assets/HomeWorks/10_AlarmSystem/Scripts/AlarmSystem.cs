4b9378ffd3dd7fb105d36c2c66f829dc65e89afb
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2024-02-14 20:56:37 +0300

    refactor HomeWork 10

--- Assets/HomeWorks/10_AlarmSystem/Scripts/AlarmSystem.cs
+++ Assets/HomeWorks/10_AlarmSystem/Scripts/AlarmSystem.cs
@@ -3,18 +3,17 @@ using UnityEngine.Events;
 
 public class AlarmSystem : MonoBehaviour
 {
-    [SerializeField] private UnityEvent _eventEnter;
-    [SerializeField] private UnityEvent _eventExit;
+    [SerializeField] private Siren _siren;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.TryGetComponent<Player>(out Player player))
-            _eventEnter.Invoke();
+            _siren.UpVolume();
     }
 
     private void OnTriggerExit(Collider collider)
     {
         if (collider.TryGetComponent<Player>(out Player player))
-            _eventExit.Invoke();
+            _siren.DownVolume();
     }
 }
