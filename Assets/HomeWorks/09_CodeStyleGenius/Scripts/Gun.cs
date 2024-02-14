9ece2e07eabc4fd0fcd57a2e1f58cb8f945c7664
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2024-02-14 12:19:40 +0300

    addad new Home work 9

---
+++ Assets/HomeWorks/09_CodeStyleGenius/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+namespace CodeStyleGenius
+{
+    public class Gun : MonoBehaviour
+    {
+        [SerializeField] private float _cooldown = 2f;
+        [SerializeField] private Bullet _bullet;
+        [SerializeField] private Transform _target;
+
+        private WaitForSeconds _sleep;
+
+        private void Awake()
+        {
+            _sleep = new WaitForSeconds(_cooldown);
+        }
+
+        private void Start()
+        {
+            StartCoroutine(Shoot());
+        }
+
+        private IEnumerator Shoot()
+        {
+            while (enabled)
+            {
+                var position = transform.position;
+                Vector3 targetDirection = (_target.position - position).normalized;
+                Bullet bullet = Instantiate(_bullet, position + targetDirection, Quaternion.identity);
+
+                bullet.SetTargetDirection(targetDirection);
+
+                yield return _sleep;
+            }
+        }
+    }
+}
